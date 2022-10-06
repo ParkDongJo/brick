aa3e04bada6b520e22a28f158784e3dd396e7c29
ParkDongJo <pdj10211@gmail.com>
2022-10-06 23:56:16 +0900

    Refactor: HeaderScrollView height Ani 수정

--- src/components/HeaderScrollView.tsx
+++ src/components/HeaderScrollView.tsx
@@ -8,38 +8,29 @@ import {
 } from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import styled from 'styled-components';
-import {HEADER_IMAGE_HEIGHT} from './../components/HeaderImage';
 
 const HeaderScrollView: React.FunctionComponent<Props> = props => {
-  const {headerMinHeight, header, onScroll} = props;
-  const headerHeight = HEADER_IMAGE_HEIGHT;
+  const {headerMinHeight, header, headerHeight, onScroll} = props;
   const scrollYAnim = useRef(new Animated.Value(0)).current;
 
   const headerHeightAni = scrollYAnim.interpolate({
-    inputRange: [0, headerHeight],
-    outputRange: [headerHeight, 0],
-    extrapolate: 'clamp',
-  });
-  const fakeViewPaddingTopAni = scrollYAnim.interpolate({
-    inputRange: [0, headerHeight],
-    outputRange: [headerHeight, 0],
+    inputRange: [0, headerHeight - headerMinHeight],
+    outputRange: [headerHeight - headerMinHeight, headerMinHeight],
     extrapolate: 'clamp',
   });
 
   return (
     <>
-      <Animated.View style={[styles.header, {height: headerHeightAni}]}>
-        {header}
-      </Animated.View>
       <RootScrollView
         bounces
-        style={{marginTop: 0}}
         scrollEventThrottle={16}
         onScroll={(event: NativeSyntheticEvent<NativeScrollEvent>) => {
           scrollYAnim.setValue(event.nativeEvent.contentOffset.y);
           onScroll(event.nativeEvent.contentOffset.y);
         }}>
-        <Animated.View style={[{paddingTop: fakeViewPaddingTopAni}]} />
+        <Animated.View style={[styles.header, {height: headerHeightAni}]}>
+          {header}
+        </Animated.View>
         {props.children}
       </RootScrollView>
     </>
@@ -48,6 +39,7 @@ const HeaderScrollView: React.FunctionComponent<Props> = props => {
 export default HeaderScrollView;
 
 type Props = {
+  headerHeight: number;
   headerMinHeight: number;
   header: React.ReactElement;
   onScroll(offsetY: number): void;
@@ -55,16 +47,8 @@ type Props = {
 
 const styles = StyleSheet.create({
   header: {
-    position: 'absolute',
-    width: '100%',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 200,
     overflow: 'hidden',
     backgroundColor: 'red',
   },
 });
-const RootScrollView = styled(ScrollView)`
-  margin-top: ${props => props.style?.marginTop};
-`;
+const RootScrollView = styled(ScrollView)``;
