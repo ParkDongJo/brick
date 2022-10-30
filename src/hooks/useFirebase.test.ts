041e0e3a4a2996da172a09b1205b0172f65c02e7
ParkDongJo <pdj10211@gmail.com>
2022-10-30 22:56:01 +0900

    Feat: useFirebase hooks test create, update, delete method 작성

--- src/hooks/useFirebase.test.ts
+++ src/hooks/useFirebase.test.ts
@@ -1,13 +1,64 @@
 import {renderHook} from '@testing-library/react-hooks';
-jest.mock('@react-native-firebase/firestore');
 import useFirebase from './useFirebase';
+jest.mock('@react-native-firebase/firestore');
 
 describe('useFirebase', () => {
   beforeEach(() => {});
-  test('fetch firestore', async () => {
+  test('fetchAll firestore', async () => {
     const {result} = renderHook(() => useFirebase());
 
-    const todos = await result.current.fetch('todos');
+    const todos = await result.current.fetchAll('todos');
     expect(todos[0].title).toBe('test');
+  });
+
+  test('fetchOne firestore', async () => {
+    const {result} = renderHook(() => useFirebase());
+
+    const todo = await result.current.fetchOne({
+      collection: 'todos',
+      docId: 'xxx',
+    });
+    expect(todo.title).toBe('test');
+  });
+
+  test('create firestore', async () => {
+    const {result} = renderHook(() => useFirebase());
+    const resp = await result.current.create({
+      collection: 'todos',
+      doc: {
+        userId: 'xx',
+        title: 'xx',
+        isDone: true,
+        isChecked: true,
+        createdAt: new Date(),
+        deadlineAt: new Date(),
+      },
+    });
+    expect(resp).not.toBeNull();
+    expect(resp).not.toBeUndefined();
+  });
+
+  test('update firestore', async () => {
+    const {result} = renderHook(() => useFirebase());
+    const resp = await result.current.update({
+      collection: 'todos',
+      docId: 'xxx',
+      data: {
+        title: 'xxx',
+      },
+    });
+    expect(resp).not.toBeNull();
+    expect(resp).not.toBeUndefined();
+  });
+
+  test('remove firestore', async () => {
+    const {result} = renderHook(() => useFirebase());
+
+    const resp = await result.current.remove({
+      collection: 'todos',
+      docId: 'xxx',
+    });
+    expect(resp).not.toBeNull();
+    expect(resp).not.toBeUndefined();
   });
 });
--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -6,7 +6,7 @@ export type FirestoreDocumentData =
   FirebaseFirestoreTypes.QuerySnapshot<FirebaseFirestoreTypes.DocumentData>;
 
 const useFirebase = () => {
-  const fetch = async (collection: string) => {
+  const fetchAll = async (collection: string) => {
     try {
       const snapshot = await firestore().collection(collection).get();
       return snapshot.docs.map(data => ({id: data.id, ...data.data()}));
@@ -14,14 +14,95 @@ const useFirebase = () => {
       return [];
     }
   };
-  const create = () => {};
-  const update = () => {};
-  const remove = () => {};
+
+  const fetchOne = async ({
+    collection,
+    docId,
+  }: {
+    collection: string;
+    docId: string;
+  }) => {
+    try {
+      const docSnapshot = await firestore()
+        .collection(collection)
+        .doc(docId)
+        .get();
+
+      if (docSnapshot.exists) {
+        return null;
+      }
+      return docSnapshot.data() || {};
+    } catch (err) {
+      console.log('test');
+      return null;
+    }
+  };
+
+  const create = async ({
+    collection,
+    doc,
+  }: {
+    collection: string;
+    doc: TodoDoc;
+  }) => {
+    try {
+      const resp = await firestore().collection(collection).add(doc);
+      return resp;
+    } catch (err) {
+      return null;
+    }
+  };
+
+  const update = async ({
+    collection,
+    docId,
+    data,
+  }: {
+    collection: string;
+    docId: string;
+    data: {[key: string]: string | number | boolean | typeof Date};
+  }) => {
+    try {
+      const resp = await firestore()
+        .collection(collection)
+        .doc(docId)
+        .update(data);
+      return resp;
+    } catch (err) {
+      return null;
+    }
+  };
+
+  const remove = async ({
+    collection,
+    docId,
+  }: {
+    collection: string;
+    docId: string;
+  }) => {
+    try {
+      const resp = await firestore().collection(collection).doc(docId).delete();
+      return resp;
+    } catch (err) {
+      return null;
+    }
+  };
+
   return {
-    fetch,
+    fetchAll,
+    fetchOne,
     create,
     update,
     remove,
   };
 };
 export default useFirebase;
+
+type TodoDoc = {
+  userId: string;
+  title: string;
+  isDone: boolean;
+  isChecked: boolean;
+  createdAt: Date;
+  deadlineAt: Date;
+};
