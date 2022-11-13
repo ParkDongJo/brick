91d9fd6d7f2b4474eccd8b3db468f2777fc5bb67
ParkDongJo <pdj10211@gmail.com>
2022-11-13 10:54:03 +0900

    Feat: implemented click ui event logic in TodoRow

--- src/components/TodoRow.tsx
+++ src/components/TodoRow.tsx
@@ -1,18 +1,28 @@
 import React, {useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {
+  Button,
+  GestureResponderEvent,
+  View,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import styled from 'styled-components';
 import TestIds from '../lib/TestIds';
 
-const Todo: React.FC<Props> = ({title, onPress}) => {
+const Todo: React.FC<Props> = ({title, onPressCheck, onPressDelete}) => {
   const [isChecked, setIsChecked] = useState(false);
   const handleClick = () => {
-    setIsChecked(true);
-    onPress();
+    setIsChecked(!isChecked);
+    onPressCheck();
+  };
+  const handleClickDeleteBtn = (event: GestureResponderEvent) => {
+    event?.preventDefault();
+    onPressDelete();
   };
   return (
     <Container accessible={true} accessibilityLabel={'listitem'}>
       <TouchableOpacity
-        testID={TestIds.TODOROW_CLICK_ROW}
+        testID={TestIds.TODOROW_TOUCH_ROW}
         onPress={handleClick}>
         {isChecked && (
           <Icon testID={TestIds.TODOROW_SHOW_CHECKICON}>
@@ -20,6 +30,7 @@ const Todo: React.FC<Props> = ({title, onPress}) => {
           </Icon>
         )}
         <Text>{title}</Text>
+        <Button onPress={handleClickDeleteBtn} title={'삭제'} />
       </TouchableOpacity>
     </Container>
   );
@@ -28,7 +39,8 @@ export default Todo;
 
 export type Props = {
   title: string;
-  onPress(): void;
+  onPressCheck(): void;
+  onPressDelete(): void;
 };
 
 const Container = styled(View)`
