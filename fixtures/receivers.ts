5ccb9f665da110656399b41e0fb34d44ebd1f408
dongjo@zigbang.com <dongjo@zigbang.com>
2022-12-03 17:28:54 +0900

    Feat: Add the fixture of receivers

---
+++ fixtures/receivers.ts
@@ -0,0 +1,17 @@
+const receivers = [
+  {
+    id: 'xxx1',
+    name: 'charles',
+    comment: 'he is my freind',
+    email: 'dongjo@gmail.com',
+    profileUrl: 'https://dongjo.pro',
+  },
+  {
+    id: 'xxx2',
+    name: 'charles',
+    comment: 'he is my freind',
+    email: 'dongjo@gmail.com',
+    profileUrl: 'https://dongjo.pro',
+  },
+];
+export default receivers;
