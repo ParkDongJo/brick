8a4e8d8422f88a77a3a3deb352d2db7338c553ac
dongjo@zigbang.com <dongjo@zigbang.com>
2022-12-04 13:02:22 +0900

    Feat: Added the ReceiverTodosScreen in UserNavigator Stack

--- src/lib/UsersStackScreens.tsx
+++ src/lib/UsersStackScreens.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ReceiversScreen from '../pages/user/ReceiversScreen';
+import ReceiverTodosScreen from '../pages/user/ReceiverTodosScreen';
 
 const Stack = createNativeStackNavigator<UsersStackScreensParamList>();
 
@@ -8,6 +9,7 @@ const UsersStackScreens = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Receivers" component={ReceiversScreen} />
+      <Stack.Screen name="ReceiverTodos" component={ReceiverTodosScreen} />
     </Stack.Navigator>
   );
 };
@@ -15,4 +17,5 @@ export default UsersStackScreens;
 
 export type UsersStackScreensParamList = {
   Receivers: undefined;
+  ReceiverTodos: undefined;
 };
