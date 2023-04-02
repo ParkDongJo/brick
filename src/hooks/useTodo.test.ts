bdc203dd0ae60542130acb6c3e02c8bb5fb7ff1f
ParkDongJo <pdj10211@gmail.com>
2023-04-02 21:57:43 +0900

    Refactor: Modified the hooks for useTodo, useUser

--- src/hooks/useTodo.test.ts
+++ src/hooks/useTodo.test.ts
@@ -1,11 +1,11 @@
 import {renderHook} from '@testing-library/react';
 import {useQuery} from '@tanstack/react-query';
-import useTodo from './useTodo';
+import useQueries from './useQueries';
 import todos from '../../fixtures/todos';
 
 jest.mock('@tanstack/react-query');
 
-describe('useTodo', () => {
+describe('useQueries', () => {
   beforeEach(() => {
     jest.useFakeTimers();
     useQuery.mockReturnValue({
@@ -16,7 +16,7 @@ describe('useTodo', () => {
   });
 
   it('When run useQueryTodo', async () => {
-    const {useQueryTodos} = useTodo();
+    const {useQueryTodos} = useQueries();
     const {result} = renderHook(() => useQueryTodos());
 
     expect(result.current.data).toHaveLength(2);
--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -1,28 +1,37 @@
-import {
-  useQuery,
-  useMutation,
-  QueryClient,
-  MutationFunction,
-} from '@tanstack/react-query';
-import {Todo} from '../store/atoms/todo';
-import {fetchAll, createOne, updateOne, removeOne} from '../lib/Firebase';
+import {UseMutationResult} from '@tanstack/react-query';
+import moment from 'moment';
 
-const useTodo = () => {
+import {Todo} from '../store/atoms/todo';
+import {FormData} from '../types';
+
+const useTodo = (mutation?: UseMutationResult) => {
+  //A tag is a string of characters like "#study #work".
+  // It extracts the words after # and turns them into an array of strings.
+  //It returns the result.
+  const convertTags = (tags: string): string[] => {
+    const result = tags.match(/#[a-zA-Z0-9가-힣]+/g);
+    if (!result) {
+      return [];
+    }
+    return result.map(tag => tag.replace('#', ''));
+  };
+  const addTodo = (newTask: FormData) => {
+    mutation?.mutate({
+      collection: 'todos',
+      doc: {
+        id: 'xxx',
+        userId: 'test',
+        title: newTask,
+        isDone: false,
+        isChecked: false,
+        createdAt: moment().format(),
+        deadlineAt: moment().add(5, 'days'),
+      } as unknown as Todo,
+    });
+  };
   return {
-    useQueryTodos: () =>
-      useQuery<Todo[]>(['todos'], {
-        queryFn: () => fetchAll('todos'),
-      }),
-    useMutaionTodo: (queryClient: QueryClient, fn: MutationFunction) =>
-      useMutation({
-        mutationFn: fn,
-        onSuccess: () => {
-          // Invalidate and refetch
-          queryClient.invalidateQueries({queryKey: ['todos']});
-        },
-      }),
+    addTodo,
+    convertTags,
   };
 };
 export default useTodo;
-
-type MutationFn = typeof createOne | typeof updateOne | typeof removeOne;
