a6457878e2ec4471adeea1bdf13d5ec592b49f69
ParkDongJo <pdj10211@gmail.com>
2023-05-04 23:42:32 +0900

    Refactor: Changed the component for todo

--- src/components/atoms/TodoRow.tsx
+++ src/components/atoms/TodoRow.tsx
@@ -7,14 +7,21 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import styled from 'styled-components';
+// import {Badge, Color} from '@react-native-material/core';
 import useTodo from '../../hooks/useTodo';
 import TestIds from '../../lib/TestIds';
-import {Todo} from '../../store/atoms/todo';
+import {Todo} from '../../types';
+import BorderSection from '../../components/atoms/BorderSection';
+import Badge from '../../components/atoms/Badge';
+import useIcon from '../../hooks/useIcon';
+import StyledText from './StyledText';
+import ProfileImage from '../../components/atoms/ProfileImage';
 
 const TodoRow: React.FC<Props> = ({datas, onPressCheck, onPressDelete}) => {
-  const {title, memo, time, tags} = datas;
+  const {title, memo, time, tags, rank} = datas;
   const [isChecked, setIsChecked] = useState(false);
   const {convertTags} = useTodo();
+  const {getIcon} = useIcon();
   const handleClick = () => {
     setIsChecked(!isChecked);
     onPressCheck();
@@ -23,19 +30,50 @@ const TodoRow: React.FC<Props> = ({datas, onPressCheck, onPressDelete}) => {
     event?.preventDefault();
     onPressDelete();
   };
+  const getRankData = (value: number) => {
+    return {
+      1: {
+        label: '급하다',
+        color: {text: '#fff', bg: '#0052CC'},
+        text: '가장 먼저 처리해봐요',
+      },
+      2: {
+        label: '중요해',
+        color: {text: '#fff', bg: '#0052CC'},
+        text: '중요하지만 급하진 않아요',
+      },
+      3: {
+        label: '오늘안에만',
+        color: {text: '#fff', bg: '#0052CC'},
+        text: '하면 좋지만, 중요하진 않아요',
+      },
+    }[value];
+  };
+
   return (
-    <TouchableOpacity testID={TestIds.TODOROW_TOUCH_ROW} onPress={handleClick}>
-      <Container accessible={true} accessibilityLabel={'listitem'}>
-        <Left>
-          <Text>{time}</Text>
-        </Left>
-        <Right>
-          <Tag>{convertTags(tags)}</Tag>
-          <Text>{title}</Text>
-          <Text>{memo}</Text>
-        </Right>
-      </Container>
-    </TouchableOpacity>
+    <BorderSection>
+      <TouchableOpacity
+        testID={TestIds.TODOROW_TOUCH_ROW}
+        onPress={handleClick}>
+        <Container accessible={true} accessibilityLabel={'listitem'}>
+          <Left>
+            <Head>
+              <ProfileImage avatar={'rabit'} radius={15} />
+              <Badge
+                label={getRankData(rank)?.label || ''}
+                color={getRankData(rank)?.color}
+              />
+              <Badge label={'40m'} color={{text: '#fff', bg: '#028759'}} />
+            </Head>
+            {/* <Icon>{getIcon('건강')}</Icon> */}
+            <StyledText text={title} fontSize={24} isLined={false} />
+          </Left>
+          <Right>
+            <StyledText text={'진행중'} color={'#808080'} fontSize={16} />
+          </Right>
+        </Container>
+      </TouchableOpacity>
+    </BorderSection>
   );
 };
 export default TodoRow;
@@ -47,24 +85,30 @@ export type Props = {
 };
 
 const Container = styled(View)`
+  position: relative;
   width: 100%;
   flex-direction: row;
-  background-color: #fff;
+  justify-content: space-between;
 `;
 const Left = styled(View)`
-  width: 100px;
-  height: 100px;
   font-size: 20px;
   font-weight: bold;
-  align-items: center;
-  justify-content: center;
+  align-items: flex-start;
 `;
 const Right = styled(View)`
   flex-direction: column;
   justify-content: center;
 `;
-const Tag = styled(Text)`
-  font-color: #eee;
-  font-size: 10px;
+const Head = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+const Msg = styled(Text)`
+  position: absolute;
+  top: -10;
+  right: 0;
+  font-size: 14px;
   font-weight: bold;
+  padding: 4px 4px;
+  color: red;
 `;
--- src/pages/todo/TodosScreen.tsx
+++ src/pages/todo/TodosScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useLayoutEffect, useEffect} from 'react';
+import React, {useRef, useLayoutEffect, useEffect, useState} from 'react';
 import {View, Text, Button} from 'react-native';
 import styled from 'styled-components';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
@@ -14,14 +14,40 @@ import {focusManager} from '@tanstack/react-query';
 import HorizontalList from '../../components/hof/HorizontalList';
 import auth from '@react-native-firebase/auth';
 import CalendarForWeek from '../../components/templates/CalendarForWeek';
+import PageModal from '../../components/organisms/PageModal';
+import CalendarForMonth from '../../components/templates/CalendarForMonth';
+import EmailInput from '../../components/organisms/AddUser';
+import UserSections from '../../components/organisms/UserSections';
+import datas from '../../../fixtures/datas';
 
 const TodosScreen: React.FC<Props> = ({navigation}) => {
   const modalRef = useRef<ModalHandle>(null);
   const {useQueryTodos} = useQueries();
+  const [modalForCalendar, setModalForCalendar] = useState(false);
+  const [modalForAddUser, setModalForAddUser] = useState(false);
+  const [modalForUsers, setModalForUsers] = useState(false);
   const {isLoading: isLoadingTodos, data: todos} = useQueryTodos(
     auth().currentUser?.uid,
   );
 
+  const closeModalForCalendar = () => {
+    setModalForCalendar(false);
+  };
+  const openModalForCalendar = () => {
+    setModalForCalendar(true);
+  };
+  const closeModalForAddUser = () => {
+    setModalForAddUser(false);
+  };
+  const openModalForAddUser = () => {
+    setModalForAddUser(true);
+  };
+  const closeModalForUsers = () => {
+    setModalForUsers(false);
+  };
+  const openModalForUsers = () => {
+    navigation.push('Users');
+  };
   const navigateToForm = () => {
     navigation.navigate('TodoForm');
   };
@@ -53,18 +79,60 @@ const TodosScreen: React.FC<Props> = ({navigation}) => {
   return (
     <Container>
       <Head>
-        <HorizontalList onPressAddBtn={() => {}}>
+        <HorizontalList
+          onPressAddBtn={openModalForAddUser}
+          onPressAll={openModalForUsers}>
           <UserList datas={users || []} />
         </HorizontalList>
       </Head>
-      <CalendarForWeek />
+      <CalendarForWeek onPressHead={openModalForCalendar} />
       <Body>
-        <TodoList todos={todos || []} />
+        <TodoList todos={datas.todos.key_user2['2023-04-06'] || []} />
       </Body>
       <Bottom>
         <BottomButton title="추가하기" onPress={navigateToForm} />
       </Bottom>
       <AlertModal ref={modalRef} />
+      <PageModal visible={modalForCalendar} close={closeModalForCalendar}>
+        <CalendarForMonth />
+      </PageModal>
+      {/* <PageModal
+        visible={modalForAddUser}
+        hasHeader={false}
+        close={closeModalForAddUser}>
+        <EmailInput
+          title={'추가할 사용자의 이메일 입력하세요.'}
+          onCancel={closeModalForAddUser}
+          onComplete={(email: string) => {
+            openModalForAddUser();
+          }}
+        />
+      </PageModal> */}
+      {/* <PageModal visible={modalForUsers} close={closeModalForUsers}>
+        <EmailInput
+          title={'추가할 사용자의 이메일 입력하세요.'}
+          onCancel={closeModalForUsers}
+          onComplete={(email: string) => {
+            openModalForAddUser();
+          }}
+        />
+        <UserSections
+          datas={[
+            {
+              title: '코치목록',
+              data: users,
+            },
+            {
+              title: '선수목록',
+              data: users,
+            },
+            {
+              title: '친구목록',
+              data: users,
+            },
+          ]}
+        />
+      </PageModal> */}
     </Container>
   );
 };
